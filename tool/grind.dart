8b8f248c156910d3b35d32b2de00c5da97529ee0
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-08-22 00:55:03 +0300

    travis config: uploadCoverage renamed to coverage

--- tool/grind.dart
+++ tool/grind.dart
@@ -4,7 +4,7 @@ import 'package:ml_tech/grind.dart' as ml_tech;
 Future<void> main(List<String> args) => grind(args);
 
 @Task()
-Future<void> uploadCoverage() => ml_tech.uploadCoverage();
+Future<void> coverage() => ml_tech.uploadCoverage();
 
 @Task()
 Future<void> test() => ml_tech.test();
