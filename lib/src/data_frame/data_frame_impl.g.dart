e9a64ad4cac1be08958a5910061c72167ec7743b
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-05-10 21:29:23 +0300

    Documentation, changelog, pubspec

--- lib/src/data_frame/data_frame_impl.g.dart
+++ lib/src/data_frame/data_frame_impl.g.dart
@@ -10,8 +10,8 @@ DataFrameImpl _$DataFrameImplFromJson(Map<String, dynamic> json) {
   return $checkedNew('DataFrameImpl', json, () {
     $checkKeys(json, allowedKeys: const ['H', 'R', 'N']);
     final val = DataFrameImpl(
-      $checkedConvert(json, 'R', (v) => (v as List)?.map((e) => e as List)),
-      $checkedConvert(json, 'H', (v) => (v as List)?.map((e) => e as String)),
+      $checkedConvert(json, 'R', (v) => (v as List)?.map((dynamic e) => e as List)),
+      $checkedConvert(json, 'H', (v) => (v as List)?.map((dynamic e) => e as String)),
       $checkedConvert(json, 'N',
           (v) => fromNumericalConverterJson(v as Map<String, dynamic>)),
     );
