abb30a7711ab0c6c76acdadd55dec9052e6d49bb
Ilya Gyrdymov <ilgyrd@gmail.com>
2022-04-30 11:21:56 +0300

    'loadPimaIndiansDiabetesDataset' function exported (#29)

--- lib/ml_dataframe.dart
+++ lib/ml_dataframe.dart
@@ -2,4 +2,5 @@ export 'package:ml_dataframe/src/data_frame/data_frame.dart';
 export 'package:ml_dataframe/src/data_frame/factories/from_csv.dart';
 export 'package:ml_dataframe/src/data_frame/factories/from_json.dart';
 export 'package:ml_dataframe/src/data_frame/factories/load_iris_dataset.dart';
+export 'package:ml_dataframe/src/data_frame/factories/load_pima_indians_diabetes_dataset.dart';
 export 'package:ml_dataframe/src/data_frame/series.dart';
